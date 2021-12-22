d4057d9e26033e48c64d193b7a9454e2a058de3e
melha <melhamra98@gmail.com>
2021-12-23 00:02:49 +0100

    cross

--- scrum-app/src/main/java/com/techtter/blog/scrum/controller/TaskController.java
+++ scrum-app/src/main/java/com/techtter/blog/scrum/controller/TaskController.java
@@ -14,7 +14,7 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/tasks")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "http://192.168.76.76:4200")
+// @CrossOrigin(origins = "http://192.168.76.76:4200")
 public class TaskController {
 
     private final TaskService taskService;
